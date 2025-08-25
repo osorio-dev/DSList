0615d8c25cdbe93fe23a3472bcea7fccdd4d4ecb
Felipe Osório <feeh.osorio.dev@gmail.com>
2025-08-24 23:48:37 -0300

    feat: create projection interface

---
+++ src/main/java/com/dev/osorio/DSList/projection/GameMinProjection.java
@@ -0,0 +1,11 @@
+package com.dev.osorio.DSList.projection;
+
+public interface GameMinProjection {
+
+    Long getId();
+    String getTitle();
+    Integer getYear();
+    String getImgUrl();
+    String getShortDescription();
+    Integer getPosition();
+}
